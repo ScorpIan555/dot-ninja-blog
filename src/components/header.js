ccef9335e4724ce0ecc1a14ed46694ce44717332
ScorpIan555 <ian.daley23@gmail.com>
2020-03-28 10:05:24 -0400

    fixed slugs, added a workspace file

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#0390fc`,
+      background: `cornflowerblue`,
       marginBottom: `1.45rem`,
     }}
   >
